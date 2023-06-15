9483a072e8744ab78027631be768683c8c1fdd69
Rafael Lindoso <rafael.lindoso@gmail.com>
2023-06-15 13:39:46 -0300

    Using ansible

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ some commands:
 * terraform init
 * terraform plan
 * terraform apply
+* chmod 600 private-key.pem
+* ansible-playbook playbook.yml -u ubuntu --private-key private-key.pem -i hosts.yml
+    * -u -> user
--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "app_test_terraform" {
   ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
   key_name      = "terraform-key"
-  user_data     = <<-EOF
-                     #!/bin/bash
-                     cd /home/ubuntu
-                     echo "<h1>Learning terraform</h1>" > index.html
-                     nohup busybox httpd -f -p 8080 &
-                     EOF
+  # user_data     = <<-EOF
+  #                    #!/bin/bash
+  #                    cd /home/ubuntu
+  #                    echo "<h1>Learning terraform</h1>" > index.html
+  #                    nohup busybox httpd -f -p 8080 &
+  #                    EOF
   tags = {
     Name = "Terraform's instance aws"
   }
