b65552302d709bc5372787ccf88fbc0b1877f157
Rafael Lindoso <rafael.lindoso@gmail.com>
2023-06-15 12:33:56 -0300

    Passing bash command to the instance

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_test_terraform" {
   ami           = "ami-024e6efaf93d85776"
   instance_type = "t2.micro"
   key_name      = "terraform-key"
+  user_data     = <<-EOF
+                     #!/bin/bash
+                     cd /home/ubuntu
+                     echo "<h1>Learning terraform</h1>" > index.html
+                     nohup busybox httpd -f -p 8080 &
+                     EOF
   tags = {
-    Name = "Terraform's instance"
+    Name = "Terraform's instance aws"
   }
 }
